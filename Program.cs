9e9c30671ffde18e480d00a11456962285346b10
Mehmet Yönügül <yonugul@gmail.com>
2022-09-22 12:58:51 +0300

    mindcodes çözümlerinin dosya isimlerini düzenleme programı olarak ayarlandı

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
-﻿namespace renamemp4
+﻿using System;
+
+namespace renamemp4
 {
     internal class Program
     {
         private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
         private static void Main(string[] args)
         {
-            var allMp4s = Directory.GetFiles(Path.Combine(BaseDirectory), "*.mp4", SearchOption.AllDirectories);
-            foreach (var mp4 in allMp4s)
+            var allMp4S = Directory.GetFiles(Path.Combine(BaseDirectory), "*.mp4", SearchOption.AllDirectories);
+            const string format = "unit-{0}-test-{1}-soru-{2}.mp4";
+            foreach (var mp4 in allMp4S)
             {
-                if (!mp4.Contains("{") || !mp4.Contains("}")) continue;
+                var folder = Path.GetFileName(Path.GetDirectoryName(mp4));
+
+                if (string.IsNullOrEmpty(folder)) continue;
                 var current = Path.GetFileName(mp4);
-                var newName = Between(mp4, "{", "}") + ".mp4";
-                var newAddr = mp4.Replace(current, newName);
-                File.Move(mp4, newAddr, true);
-                Console.WriteLine($"{mp4} - {newAddr} olarak değiştirildi.");
+                var  unit = folder[..1];
+                if (!int.TryParse(unit, out var unitInt)) continue;
+                if (mp4.Contains("Quiz"))
+                {
+                    var qo = mp4[^6..].Replace(".mp4", "").Trim();
+                    var newName = string.Format(format, unitInt, 2, qo);
+                    var newAddr = mp4.Replace(current, newName);
+                    File.Move(mp4, newAddr, true);
+                    Console.WriteLine($"{mp4} - {newAddr} olarak değiştirildi.");
+                }
+                else
+                {
+                    var qo = mp4[^6..].Replace(".mp4","").Trim();
+                    var newName = string.Format(format, unitInt, 1, qo);
+                    var newAddr = mp4.Replace(current, newName);
+                    File.Move(mp4, newAddr, true);
+                    Console.WriteLine($"{mp4} - {newAddr} olarak değiştirildi.");
+                }
             }
 
             Console.WriteLine("DONE!");
